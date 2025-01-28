84adc33c0e121f23bd3444b1bdec8738d0f83171
pavlovmu <pavlovm333@gmail.com>
2025-01-28 23:33:42 +0300

    Revert "ДЗ_33"
    This reverts commit bfd87818185cf6b11792c920f148f5b0a35e1e3f.

--- src/main/java/ru/otus/http/jserver/HttpServer.java
+++ src/main/java/ru/otus/http/jserver/HttpServer.java
@@ -3,8 +3,6 @@ package ru.otus.http.jserver;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
@@ -16,34 +14,18 @@ public class HttpServer {
     }
 
     public void start() {
-        ExecutorService serv = Executors.newFixedThreadPool(5);
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            while (true) {
-                Socket socket = serverSocket.accept();
-                serv.execute(() -> {
-                    try {
-                        System.out.println("Подключился новый клиент в потоке " + Thread.currentThread().getName());
-                        byte[] buffer = new byte[8192];
-                        int n = socket.getInputStream().read(buffer);
-                        HttpRequest request = new HttpRequest(new String(buffer, 0, n));
-                        request.info(true);
-                        dispatcher.execute(request, socket.getOutputStream());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        try {
-                            socket.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+            try (Socket socket = serverSocket.accept()) {
+                System.out.println("Подключился новый клиент");
+                byte[] buffer = new byte[8192];
+                int n = socket.getInputStream().read(buffer);
+                HttpRequest request = new HttpRequest(new String(buffer, 0, n));
+                request.info(true);
+                dispatcher.execute(request, socket.getOutputStream());
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            serv.shutdown();
         }
     }
 }
